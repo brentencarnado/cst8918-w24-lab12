79a80f03714e78a4f3eccc6cca1cce20f876c019
Brent <brent.encarnado98@gmail.com>
2024-04-01 15:57:21 -0400

    removed duplicate terraform configurations

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,16 +1,6 @@
 terraform {
   required_version = "~> 1.5"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.96.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
 }
 
 
